bdcf858a76d0ce9f94bf50894a06e4893603fea1
Tom Draper <tomjdraper1@gmail.com>
2024-01-31 17:31:45 +0000

    refactor

--- Pages/Upload.razor.cs
+++ Pages/Upload.razor.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Text;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
@@ -102,9 +103,6 @@ namespace visual_binary_analysis.Pages
         private FileData fileData;
         private string? ErrorMessage;
 
-        private string? SearchText { get; set; }
-
-
         void OnDragEnter(DragEventArgs e) => HoverClass = "hover";
 
         void OnDragLeave(DragEventArgs e) => HoverClass = string.Empty;
@@ -123,12 +121,12 @@ namespace visual_binary_analysis.Pages
         {
             ErrorMessage = string.Empty;
 
-            var file = e.File;
+            IBrowserFile file = e.File;
             long maxFileSize = 1024L * 1024L * 1024L * 2L;
             using var stream = file.OpenReadStream(maxAllowedSize: maxFileSize);
-            using var ms = new MemoryStream();
+            using MemoryStream ms = new();
             await stream.CopyToAsync(ms);
-            var fileBytes = ms.ToArray();
+            byte[] fileBytes = ms.ToArray();
             fileData = new FileData(fileBytes);
 
             // source = $"data:{file.ContentType};base64,{Convert.ToBase64String(fileBytes)}";
@@ -139,22 +137,20 @@ namespace visual_binary_analysis.Pages
         private async Task OnHover(string source, int index)
         {
             if (index > FileData.PageSize)
-            {
                 return;
-            }
             switch (source)
             {
                 case "hex":
-                    await JSRuntime.InvokeVoidAsync("addActive", "text-" + index.ToString());
-                    await JSRuntime.InvokeVoidAsync("addActive", "byte-" + index.ToString());
+                    await JSRuntime.InvokeVoidAsync("addClass", "text-" + index.ToString(), "active");
+                    await JSRuntime.InvokeVoidAsync("addClass", "byte-" + index.ToString(), "active");
                     break;
                 case "text":
-                    await JSRuntime.InvokeVoidAsync("addActive", "hex-" + index.ToString());
-                    await JSRuntime.InvokeVoidAsync("addActive", "byte-" + index.ToString());
+                    await JSRuntime.InvokeVoidAsync("addClass", "hex-" + index.ToString(), "active");
+                    await JSRuntime.InvokeVoidAsync("addClass", "byte-" + index.ToString(), "active");
                     break;
                 case "byte":
-                    await JSRuntime.InvokeVoidAsync("addActive", "hex-" + index.ToString());
-                    await JSRuntime.InvokeVoidAsync("addActive", "text-" + index.ToString());
+                    await JSRuntime.InvokeVoidAsync("addClass", "hex-" + index.ToString(), "active");
+                    await JSRuntime.InvokeVoidAsync("addClass", "text-" + index.ToString(), "active");
                     break;
             }
         }
@@ -162,22 +158,20 @@ namespace visual_binary_analysis.Pages
         private async Task OnLeave(string source, int index)
         {
             if (index > FileData.PageSize)
-            {
                 return;
-            }
             switch (source)
             {
                 case "hex":
-                    await JSRuntime.InvokeVoidAsync("removeActive", "text-" + index.ToString());
-                    await JSRuntime.InvokeVoidAsync("removeActive", "byte-" + index.ToString());
+                    await JSRuntime.InvokeVoidAsync("removeClass", "text-" + index.ToString(), "active");
+                    await JSRuntime.InvokeVoidAsync("removeClass", "byte-" + index.ToString(), "active");
                     break;
                 case "text":
-                    await JSRuntime.InvokeVoidAsync("removeActive", "hex-" + index.ToString());
-                    await JSRuntime.InvokeVoidAsync("removeActive", "byte-" + index.ToString());
+                    await JSRuntime.InvokeVoidAsync("removeClass", "hex-" + index.ToString(), "active");
+                    await JSRuntime.InvokeVoidAsync("removeClass", "byte-" + index.ToString(), "active");
                     break;
                 case "byte":
-                    await JSRuntime.InvokeVoidAsync("removeActive", "hex-" + index.ToString());
-                    await JSRuntime.InvokeVoidAsync("removeActive", "text-" + index.ToString());
+                    await JSRuntime.InvokeVoidAsync("removeClass", "hex-" + index.ToString(), "active");
+                    await JSRuntime.InvokeVoidAsync("removeClass", "text-" + index.ToString(), "active");
                     break;
             }
         }
@@ -206,9 +200,73 @@ namespace visual_binary_analysis.Pages
             StateHasChanged();
         }
 
-        public void Search()
+        // public void Search()
+        // {
+        //     Console.WriteLine("hi");
+        //     Console.WriteLine(SearchText);
+        // }
+        // private async Task Search(Microsoft.AspNetCore.Components.ChangeEventArgs patharg)
+        // {
+        //     var path = (string)patharg.Value;
+        //     Console.WriteLine(path);
+        // }
+
+        private readonly List<int> HighlightedChars = [];
+
+        private async Task ClearHighlight()
         {
-            Console.WriteLine(SearchText);
+            if (HighlightedChars.Count == 0)
+                return;
+            for (int i = 0; i < HighlightedChars.Count; i++)
+            {
+                int index = HighlightedChars[i];
+                await JSRuntime.InvokeVoidAsync("removeHighlight", "text-" + index.ToString());
+            }
+            HighlightedChars.Clear();
+        }
+
+        private async Task HighlightRange(int start, int length)
+        {
+            if (length == 0)
+                return;
+
+            for (int i = start; i < start + length; i++)
+            {
+                if (i == start || i == 0)
+                    await JSRuntime.InvokeVoidAsync("addClass", "text-" + i.ToString(), "highlight-start");
+                if (i == start + length - 1)
+                    await JSRuntime.InvokeVoidAsync("addClass", "text-" + i.ToString(), "highlight-end");
+                if (i != start && i != start + length - 1)
+                    await JSRuntime.InvokeVoidAsync("addClass", "text-" + i.ToString(), "highlight");
+                HighlightedChars.Add(i);
+            }
+        }
+
+        private async Task OnChange(ChangeEventArgs e)
+        {
+            await ClearHighlight();
+            if (e.Value == null)
+                return;
+            string? searchText = e.Value.ToString();
+            if (searchText == null)
+                return;
+
+            for (int i = 0; i < fileData.Pages.Count; i++)
+            {
+                FileData.Page page = fileData.Pages[i];
+                string text = page.Text();
+                int index = text.IndexOf(searchText);
+                if (index == -1)
+                    continue;
+
+                if (i != fileData.PageIndex)
+                {
+                    fileData.SetPage(i);
+                    StateHasChanged();
+                }
+                await HighlightRange(index, searchText.Length);
+                return;
+            }
         }
     }
 }
--- wwwroot/js/util.js
+++ wwwroot/js/util.js
@@ -1,7 +1,13 @@
-function addActive(id) {
-  document.getElementById(id)?.classList.add("active");
+function addClass(id, className) {
+  document.getElementById(id)?.classList.add(className);
 }
 
-function removeActive(id) {
-  document.getElementById(id)?.classList.remove("active");
+function removeClass(id, className) {
+  document.getElementById(id)?.classList.remove(className);
+}
+
+function removeHighlight(id) {
+  document.getElementById(id)?.classList.remove('highlight');
+  document.getElementById(id)?.classList.remove('highlight-start');
+  document.getElementById(id)?.classList.remove('highlight-end');
 }
