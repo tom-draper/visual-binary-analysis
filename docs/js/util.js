c769e2eb8c9aaded9c64788a126ef11e733de91e
Tom Draper <tomjdraper1@gmail.com>
2024-01-31 20:16:07 +0000

    build

--- docs/js/util.js
+++ docs/js/util.js
@@ -5,3 +5,17 @@ function addActive(id) {
 function removeActive(id) {
   document.getElementById(id)?.classList.remove("active");
 }
+
+function addClass(id, className) {
+  document.getElementById(id)?.classList.add(className);
+}
+
+function removeClass(id, className) {
+  document.getElementById(id)?.classList.remove(className);
+}
+
+function removeHighlight(id) {
+  document.getElementById(id)?.classList.remove("highlight");
+  document.getElementById(id)?.classList.remove("highlight-start");
+  document.getElementById(id)?.classList.remove("highlight-end");
+}
